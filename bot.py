07a6cda5bbd84a9552ec3583f405450b61c3b4ce
09B23520 <pcgb6fve@s.okayama-u.ac.jp>
2025-05-07 21:46:51 +0900

    init

---
+++ bot.py
@@ -0,0 +1,33 @@
+import discord
+import requests
+from config import DISCORD_TOKEN
+
+url = "http://localhost:8000/generate"
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print("ready...")
+    
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    text = message.content
+    print("Input: "+text)
+    payload = {
+        "prompt": text
+    }
+    
+    response = requests.post(url, json=payload)
+    output = response.json()["text"]
+
+    if response.status_code==200:
+        await message.channel.send(output)
+    else:
+        await message.channel.send("すみません．通信が悪いみたいです...")
+        
+client.run(DISCORD_TOKEN)
---
+++ gemma_server.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from llama_cpp import Llama
+from fastapi import FastAPI, Request
+from config import MODEL_PATH, SYS_MSG, SYS_MSG_REPLY
+
+# メッセージ履歴の制限
+max_messages = 10
+
+app = FastAPI()
+
+base_dir = Path(__file__).resolve().parent
+model_path = base_dir / MODEL_PATH
+
+llm = Llama(
+    model_path=str(model_path),
+    n_ctx=2000,
+)
+
+messages = []
+# gemmaはシステムロールなし&ユーザー，アシスタントの繰り返ししか許可しないため
+messages.append({"role": "user", "content": SYS_MSG})
+messages.append({"role": "assistant", "content": SYS_MSG_REPLY})
+
+@app.post("/generate")
+async def generate_text(request: Request):
+    data = await request.json()
+    user_input = data.get("prompt")
+    
+    messages.append({"role": "user", "content": user_input})
+
+    # 推論
+    output = llm.create_chat_completion(
+        messages,
+        stop=["###","</s>","�"]
+    )
+    
+    print(output)
+    messages.append(output["choices"][0]["message"])
+
+    while len(messages) > max_messages:
+        messages.pop(2) # 先頭のユーザーメッセージを削除(システムプロンプト除く)
+    print(messages)
+    return {"text": output["choices"][0]["message"]["content"]}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
